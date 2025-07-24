9d4e70d9b8cd4e3edf4b9c7161580e627d8ebd4a
Khrystyna <khrystyna.terletska@outlook.it>
2025-07-24 14:26:11 +0200

    dotenv added

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.scss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
+    new Dotenv(),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
-  mode: 'development',
+  mode: "development",
   devServer: {
-    static: './dist',
+    static: "./dist",
     open: true,
     hot: true,
   },
